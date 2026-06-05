3f3f82d4bc5e1390b5c1d5b072c311b30e792bd9
Mati Novelli <msancheznovelli@itba.edu.ar>
2026-06-04 22:06:21 -0300

    fix: minor change to avoid warnings

--- Userland/c/commands/cat.c
+++ Userland/c/commands/cat.c
@@ -3,7 +3,7 @@
 #include "commands/commands.h"
 
 // cat: copia stdin a stdout hasta EOF. Pensado para pipes (ej. cat | filter).
-void cat_main(int argc, char *argv[]){
+void cat_main(int argc, char **argv){
     for(int i = 0; i < argc; i++) {
         sys_write(STDOUT, argv[i], sizeof(argv)+1);
     }
