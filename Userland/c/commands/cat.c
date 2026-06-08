08d126f15701073f336b90bbd59708a987f42920
Mati Novelli <msancheznovelli@itba.edu.ar>
2026-06-07 23:38:16 -0300

    fix: cat command

--- Userland/c/commands/cat.c
+++ Userland/c/commands/cat.c
@@ -2,13 +2,22 @@
 #include "lib/userlib.h"
 #include "commands/commands.h"
 
+#define EOF 0
+
 // cat: copia stdin a stdout tal cual hasta EOF. Pensado para pipes (ej. cat | filter).
 void cat_main(int argc, char **argv){
     (void)argc; (void)argv;
+    int c;
     char buf[128];
-    uint64_t n;
-    while((n = read_full(buf, sizeof(buf))) > 0){
-        sys_write(STDOUT, buf, n);
-    }
-    sys_exit(0);
+    uint16_t count = 0;
+    while( (c = getchar()) != EOF) {
+        print("%c", c);
+        buf[count++] = c;
+        if (c == '\n' || count - 1 >= 128) {
+            buf[count] = 0;
+            print("%s", buf);
+            count = 0;
+        }
+    } 
+    sys_exit(0); 
 }
