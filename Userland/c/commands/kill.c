7aca59e8e4b021f5d4e087acad22affe915c7fa1
Mati Novelli <msancheznovelli@itba.edu.ar>
2026-06-26 02:16:19 -0300

    feat(kill): handle multiple arguments

--- Userland/c/commands/kill.c
+++ Userland/c/commands/kill.c
@@ -3,13 +3,19 @@
 #include "shell/shell.h"
 #include "commands/commands.h"
 
-// kill <pid>: mata el proceso indicado.
+// kill <pid> [pid ...]: mata los procesos indicados.
 void kill_cmd(void){
     const char *a = cmd_args();
-    int64_t pid = (a != 0) ? next_uint(&a) : -1;
-    if(pid <= 0){
-        shellPrintString("uso: kill <pid>\n");
+    if(a == 0){
+        shellPrintString("uso: kill <pid> [pid ...]\n");
         return;
     }
-    sys_kill((uint64_t)pid);
+    int enviados = 0;
+    int64_t pid;
+    while((pid = next_uint(&a)) > 0){
+        sys_kill((uint64_t)pid);
+        enviados++;
+    }
+    if(enviados == 0)
+        shellPrintString("uso: kill <pid> [pid ...]\n");
 }
